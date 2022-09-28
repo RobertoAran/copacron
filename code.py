b5f835fde70bab0b5d0534b5849b235b8a2ce972
RobertoAran <robertoaranda133@gmail.com>
2022-09-28 12:39:00 +0200

    Created def createDir, copyFile, createFile and added logic to scanOrigin

--- code.py
+++ code.py
@@ -4,6 +4,15 @@ import pathlib
 import sys
 import shutil
 
+def createDir(destination):
+    os.mkdir(destination)
+
+def copyFile(origin, destination):
+    shutil.copy(origin, destination)
+
+def createFile(origin, destination):
+    shutil.copy(origin, destination)
+
 
 def hash_file(filename):
     h = hashlib.sha1()
@@ -14,27 +23,42 @@ def hash_file(filename):
             h.update(chunk)
     return h.hexdigest()
 
+def scanDestinyDelete():
+    print("deleted")
 
-def scanDestiny(destiniDir):
-    if os.path.exists(destiniDir):
-        if destiniDir.is_file():
-            return hash_file(destiniDir)
+def scanDestiny(destination, is_file):
+    if os.path.exists(destination):
+        if is_file:
+            return hash_file(destination)
         return "dir"
     return "none"
 
-
-def scanOrigin(arguments, actualRute):
-    with os.scandir(arguments[0]) as ficheros:
+# Scan original route file per file
+# call to scanDestiny to comprobate if the file or dir are in the destiny or not
+# Recolect hash of origin file and destiny file and if is necesary send to copyFile
+def scanOrigin(origin, destiny):
+    with os.scandir(origin) as ficheros:
+        actualSize = len(ficheros)
         for fichero in ficheros:
-            originDir = arguments[0] + '/' + fichero.name
-            destiniDir = arguments[1] + '/' + fichero.name
+            origin = origin + '/' + fichero.name
+            destination = destination + '/' + fichero.name
             if fichero.is_file():
-                scanDestiny()
-            if fichero.is_dir():
-                scanOrigin(destiniDir)
-
+                response = scanDestiny(destination, True)
+                if response != "none" and hash_file(origin) != response:
+                    copyFile(origin, destination)
+                else:
+                    createFile(origin, destination)
+            else:
+                response = scanDestiny(destination, False)
+                if response == "dir":
+                    scanOrigin(origin, destiny)
+                else:
+                    createDir(destination)
+                    scanOrigin(origin, destiny)
 
 if __name__ == '__main__':
     # arguments = sys.argv[1:]
     arguments = ['/home/roberto/PycharmProjects/Crontab', '/home/roberto/PycharmProjects/Cronta']
-    scanOrigin(arguments)
+    origin = arguments[0]
+    destiny = arguments[1]
+    scanOrigin(origin, destiny)
