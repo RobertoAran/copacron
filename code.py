26df39c6118e66e9cfbc8b8cb3d9bb8aede11b9f
RobertoAran <robertoaranda133@gmail.com>
2022-09-28 10:59:14 +0200

    Added hash_file function to code file.

--- code.py
+++ code.py
@@ -1,14 +1,29 @@
+import hashlib
 import os
-# import sys
+import sys
+import shutil
+
+
+def hash_file(filename):
+    h = hashlib.sha1()
+    with open(filename, 'rb') as file:
+        chunk = 0
+        while chunk != b'':
+            chunk = file.read(1024)
+            h.update(chunk)
+    return h.hexdigest()
+
 
 def scandir(arguments):
-    ejemplo_dir = '/home/roberto/PycharmProjects/Crontab'
-    with os.scandir(ejemplo_dir) as ficheros:
+    with os.scandir(arguments) as ficheros:
         for fichero in ficheros:
-            if fichero.is_file() or fichero.is_dir():
+            if fichero.is_file():
                 print(fichero.name)
+            if fichero.is_dir():
+                nameCarpet = arguments + '/' + fichero.name
+                scandir(nameCarpet)
 
 if __name__ == '__main__':
     # arguments = sys.argv[1:]
-    arguments = '/home/roberto'
+    arguments = '/home/roberto/PycharmProjects/Crontab'
     scandir(arguments)
